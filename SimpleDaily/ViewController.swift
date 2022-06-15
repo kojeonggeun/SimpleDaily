efef154f61c207e2c294c53f9faa1cdd38ede6e6
kojunggun <ggggg46@naver.com>
2022-06-15 15:54:48 +0900

    Start SimpleDaily

---
+++ SimpleDaily/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SimpleDaily
+//
+//  Created by 고정근 on 2022/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
